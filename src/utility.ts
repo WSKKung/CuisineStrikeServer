765b631ee683218147e82fd64f38bbc3827ef7f8
WSKKung <armkung493@gmail.com>
2024-01-03 21:59:19 +0700

    Add array shuffling & object shallow clone func

--- src/utility.ts
+++ src/utility.ts
@@ -30,4 +30,34 @@ namespace Utility {
 		return (mask1 & mask2) === mask2;
 	}
 	
+	/**
+	 * Shuffle contents of given array using Fisher–Yates shuffle algorithm
+	 * This function mutates the given array.
+	 * @param array 
+	 * @returns 
+	 */
+	export function shuffle<T>(array: Array<T>): Array<T> {
+		let currentIndex: number = array.length;
+		let randomIndex: number;
+		  
+		// While there remain elements to shuffle.
+		while (currentIndex > 0) {
+	  
+		  // Pick a remaining element.
+		  randomIndex = Math.floor(Math.random() * currentIndex);
+		  currentIndex--;
+	  
+		  // And swap it with the current element.
+		  [array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+		}
+	  
+		return array;
+	}
+
+	export function shallowClone(object: any): any {
+		let cloned: any = {};
+		Object.keys(object).forEach(key => cloned[key] = object[key]);
+		return cloned;
+	}
 }
