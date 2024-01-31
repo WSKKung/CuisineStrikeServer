157e2328bd48575874808fbc61a4679226581ace
WSKKung <armkung493@gmail.com>
2024-01-31 14:56:01 +0700

    Add card effect

--- src/effects/effect.ts
+++ src/effects/effect.ts
@@ -1,7 +1,34 @@
-export interface Effect {
-	
+import { Card } from "../card"
+import { GameState } from "../match"
+
+export interface CardEffect {
+	type: "active" | "passive"
+	condition: CardEffectConditionFunction
+	activate: CardEffectActivateFunction
 }
 
-export namespace Effect {
+export type CardEffectConditionFunction = (context: CardEffectContext) => boolean
+export type CardEffectActivateFunction = (context: CardEffectContext) => Promise<void>
 
+export interface CardEffectContext {
+	state: GameState,
+	card: Card,
+	player: string
+}
+
+export const CardEffectProvider = {
+
+	effects: {} as {[code: number]: CardEffect},
+
+	register(code: number, effect: CardEffect) {
+		this.effects[code] = effect
+	},
+
+	hasEffect(code: number): boolean {
+		return !!this.effects[code]
+	},
+
+	getEffect(code: number): CardEffect | null {
+		return this.effects[code] || null
+	}
 }
