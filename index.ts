ca3f926c5ebf697f1c232f132ec6e72df8d66c77
WSKKung <armkung493@gmail.com>
2024-01-31 14:59:04 +0700

    Add dump state RPC

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
+import { CardEffectProvider } from "./src/effects/effect";
 import { matchHandler, matchmakerMatched } from "./src/match_handler";
-import { recipeCheckRPC } from "./src/test/test_rpcs";
+import { registerCardEffectScripts } from "./src/scripts";
+import { dumpGameStateRPC, recipeCheckRPC } from "./src/test/test_rpcs";
 
 const InitModule: nkruntime.InitModule = function(ctx, logger, nk, initializer) {
 	logger.info("Typescript Runtime initializing");
@@ -7,13 +9,15 @@ const InitModule: nkruntime.InitModule = function(ctx, logger, nk, initializer) 
 	logger.info("BeforeMatchmakerAdd hook registered");
 	initializer.registerMatch("lobby", matchHandler);
 	logger.info("MatchHandler registered");
-	initializer.registerMatchmakerMatched(matchmakerMatched)
+	initializer.registerMatchmakerMatched(matchmakerMatched);
 	logger.info("MatchmakerMatched hook registered");
 	logger.info("Typescript Runtime ready, les go, woo!!!");
 	initializer.registerStorageIndex("cards", "cards", undefined, ["name", "type", "description", "class", "grade", "power", "health"], 1000, false);
+	initializer.registerBeforeAuthenticateGoogle(beforeAuthenticateLogin);
 	//registerTestRPCs(initializer);
 	// Registers every testing RPC functions into the server
 	initializer.registerRpc("recipe_check", recipeCheckRPC);
+	initializer.registerRpc("dump_state", dumpGameStateRPC);
 };
 
 // Modify client matchmaking request
@@ -22,6 +26,10 @@ const beforeMacthmakerAdd: nkruntime.RtBeforeHookFunction<nkruntime.EnvelopeMatc
 	envelope.matchmakerAdd.maxCount = 2;
 	return envelope;
 };
+
+const beforeAuthenticateLogin: nkruntime.BeforeHookFunction<nkruntime.AuthenticateGoogleRequest> = function(ctx, logger, nk, data) {
+	return data;
+}
 
 // Reference InitModule to avoid it getting removed on build
 !InitModule || InitModule.bind(null);
