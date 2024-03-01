bf8a6d29c370815b0c48d60930f1aa73eeb939c2
WSKKung <armkung493@gmail.com>
2024-03-01 22:44:28 +0700

    Add string key for client translation

---
+++ src/communications/string_keys.ts
@@ -0,0 +1,15 @@
+export enum EndResultStringKeys {
+	GENERIC = "generic",
+	HP_REACHES_ZERO = "hp_reaches_zero",
+	DECKED_OUT = "decked_out",
+	TIMEOUT = "timeout",
+	DISCONNECTED = "disconnected"
+}
+
+export enum SelectionHintStringKeys {
+	GENERIC = "select.generic",
+	CARDS = "select.cards",
+	ZONES = "select.zones",
+	YES_NO = "select.yes_no",
+	OPTION = "select.option"
+}
