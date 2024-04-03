c0b4746695ffe30665412dfa9434852ba56fc77a
WSKKung <armkung493@gmail.com>
2024-04-03 21:47:14 +0700

    Add add coin RPC

--- src/controllers/card_collection.ts
+++ src/controllers/card_collection.ts
@@ -7,6 +7,10 @@ const RPCSchemas = {
 	addToCollection: z.object({
 		playerId: z.string(),
 		cards: z.array(z.number().int())
+	}),
+	addCoin: z.object({
+		playerId: z.string(),
+		coin: z.number().int()
 	})
 }
 
@@ -31,6 +35,25 @@ export const addToCollectionRPC: nkruntime.RpcFunction = function(ctx, logger, n
 
 	let storage = NakamaAdapter.storageAccess({ nk, logger });
 	storage.addCardToPlayerCollection(args.playerId, args.cards.map(code => ({ id: nk.uuidv4(), code: code })));
+
+	return JSON.stringify({ success: true });
+}
+
+export const addCoinRPC: nkruntime.RpcFunction  = function(ctx, logger, nk, payload) {
+	guardSystemOnly(ctx, logger, nk, payload);
+	let payloadParseResult = RPCSchemas.addCoin.safeParse(JSON.parse(payload));
+	if (!payloadParseResult.success) {
+		throw new Error("Invalid argument");
+	}
+
+	let args = payloadParseResult.data;
+
+	if (nk.usersGetId([args.playerId]).length === 0) {
+		throw new Error("Target player does not exist")
+	}
+
+	let storage = NakamaAdapter.storageAccess({ nk, logger });
+	storage.givePlayerCoin(args.playerId, args.coin);
 
 	return JSON.stringify({ success: true });
 }
